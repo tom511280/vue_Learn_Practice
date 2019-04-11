b91c7686f14179a5ca38a9ddd7c35ebd391b2e72
tom511280 <tom511280@gmail.com>
2019-04-11 17:38:56 +0800

    標題:新增學習範例
    1.新增2019/04/11_1530 - 2019/04/11_1730 學習範例

---
+++ vuePractice/vuePractice_ProjectExample/vueProject/src/components/footbar.vue
@@ -0,0 +1,10 @@
+<template>
+     <!--尾部-->
+    <div class="foot-btn" >
+      <ul>
+        <li class="home"><a href="index_m.html"></a></li>
+        <li class="write"><a href="javascript:;"></a></li>
+        <li class="my"><a href="mydoc_m.html"></a></li>
+      </ul>
+    </div>
+</template>
---
+++ vuePractice/vuePractice_ProjectExample/vueProject/src/components/home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home">
+    <!--banner-->
+    <slider></slider>
+    <!--home列表-->
+    <list></list>
+    </div>
+</template>
+<script>
+import slider from './slider.vue';
+import list from './list.vue';
+
+export default {
+    components:{
+        slider,list
+    }
+}
+</script>
